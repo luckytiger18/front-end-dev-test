9db2acc4e921e22e1666b3ca44816975bc740859
Annie Luc <lannie3186@gmail.com>
2020-08-04 21:57:04 -0700

    - Added events to the calendar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 
-
 console.log("working")
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth'
+    initialView: 'dayGridMonth',
+    events: [
+        {
+          title: 'Adventure Walk',
+          start: '2020-08-06T14:00:00'
+        },
+        {
+            title: 'Park Date',
+            start: '2020-08-05T15:00:00'
+          },
+        {
+            title: 'Pooped',
+            start: '2020-08-04T07:30:00',
+          },
+          {
+            title: 'Pooped',
+            start: '2020-08-04T19:30:00',
+          },
+          
+    ]
   });
   calendar.render();
 });
