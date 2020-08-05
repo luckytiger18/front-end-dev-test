ebb174c236857783391cb80bc3005bac132ee2f2
Annie Luc <lannie3186@gmail.com>
2020-08-04 21:30:08 -0700

    - Fixed error on image - Added Popcorn's activity calendar

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-import Home from '../index.html'
-import DesktopStyles from './scss/app-desktop.scss'
-import TabletStyles from './scss/app-tablet.scss'
-import MobileStyles from './scss/app-mobile.scss'
-import 'bootstrap'
+
 
 console.log("working")
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth'
+  });
+  calendar.render();
+});
